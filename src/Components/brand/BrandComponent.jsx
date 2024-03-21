788f3fb1a37aa471f501c2c9247cbc06a22e1c47
Mahdiar <mahdyarvaez@gmail.com>
2024-03-21 07:25:49 -0700

    brand

--- src/Components/brand/BrandComponent.jsx
+++ src/Components/brand/BrandComponent.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import "./brand.css";
+import { google, atlassian, dropbox, shopify, slack } from "./import";
 export default function Brand() {
   return (
-    <div>Brand</div>
-  )
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+    </div>
+  );
 }
