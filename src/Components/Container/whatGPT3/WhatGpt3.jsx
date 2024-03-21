b712b25be190b0edadd9d7e902f505f6a1211e74
Mahdiar <mahdyarvaez@gmail.com>
2024-03-21 10:12:49 -0700

    what gpt3
    styles and react

--- src/Components/Container/whatGPT3/WhatGpt3.jsx
+++ src/Components/Container/whatGPT3/WhatGpt3.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import FeatureComponent from "../../feature/Feature";
+import './gpt3.css'
 export default function WhatGpt3() {
   return (
-    <div>WhatGpt3</div>
-  )
+    <div id="wgpt3" className="gpt3__whatgpt3 section-margin">
+      <div className="gpt3__whatgpt3-features">
+        <FeatureComponent title={'What is GPT-3'} text={'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'}/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <FeatureComponent title={'Chatbots'} text={'We so opinion friends me message as delight. Whole front do of plate heard oh ought. '}/>
+        <FeatureComponent title={'Knowledgebase'} text={'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'}/>
+        <FeatureComponent title={'Education' }text={'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'}/>
+        
+      </div>
+    </div>
+  );
 }
--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './feature.css'
-export default function feature() {
-  return (
-    <div>feature</div>
+export default function FeatureComponent({title,text}) {
+  return ( 
+    <div className='gpt3__features-container '>
+        <div className='gpt3__features-container__features-title'>
+          <div/>
+          <h1>{title}</h1>
+        </div>
+        <div className='gpt3__features-container__features-text'>
+          <p>{text}</p>
+        </div>
+    </div>
   )
 }
